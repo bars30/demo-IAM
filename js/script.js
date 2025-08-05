b7096ecd62ed667e16196181fddad8c5993157a8
anna3bars <154560345+anna3bars@users.noreply.github.com>
2025-08-05 15:52:34 +0400

    cleaned main js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,14 @@
-import {
-  updateUIText,
-  getBotReply,
-  getCurrentLang,
-  setCurrentLang
-} from "./lang-handler.js";
-
-import { 
-  questionsBtn, 
-  promptsSection,
-  chatboxMessages,
-  input,
-  langButtons
-  
-} from "./dom-elements.js";
+import { questionsBtn } from "./dom-elements.js";
 import { setupThemeToggle } from './theme-toggler.js';
-import { restoreChatHistory, saveChatHistory } from './chat-history.js';
-import { addMessage, typeTextHTML, typeText } from './message-renderer.js';
-
-import { initFileManager, getSelectedFiles, clearSelectedFiles, updatePreview,
-  updateFileList } from './file-manager.js';
-
+import { restoreChatHistory } from './chat-history.js';
+import { initFileManager } from './file-manager.js';
 import { setupLanguageSwitcher } from './language-switcher.js';
 import { setupChatController } from './chat-controller.js';
-
 import { setupPromptButtons } from "./prompt-buttons.js";
 import { setupLangButtonListeners } from "./lang-button-listeners.js";
 import { setupQuickPromptsToggle } from "./quick-prompts-toggle.js";
 import { setupTextareaAutosize } from './input-autoresize.js';
+import { setupClearChatButtons } from './clear-chat.js';
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -35,36 +17,15 @@ document.addEventListener("DOMContentLoaded", () => {
   restoreChatHistory();
   initFileManager();
   setupChatController();
-setupLangButtonListeners();
+  setupLangButtonListeners();
   
   const { langSwitcher } = setupLanguageSwitcher();
-
   const footerBtn = document.querySelectorAll(".footer-btn-cont button");
-  console.log(langSwitcher);
- 
-  let firstMessage = true;
-
-setupQuickPromptsToggle(questionsBtn);
-
+  setupQuickPromptsToggle(questionsBtn);
   const textarea = document.getElementById('chatbox-input');
   setupTextareaAutosize(textarea);
-
-  let chatState = "waitingUserQuestion"; 
-
-  const chatboxInput = document.getElementById("chatbox-input");
   const sendBtn = document.querySelector(".chatbox-send-btn");
-
   setupPromptButtons(langSwitcher, sendBtn, footerBtn);
-
-  const clearBtns = document.querySelectorAll('.chatbox-footer-btn.clear-btn');
-
-  clearBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      
-      localStorage.removeItem('chatHistory');
-      location.reload();
-    });
-  });
-
+  setupClearChatButtons();
 
 });
